3182a3de476b7dfc3893cba8807adf3b0eb59aa6
simondelamarre <landscape.viewer@gmail.com>
2021-03-07 11:09:33 +0100

    exclude until tags attributes

--- src/parsers/Attributes.ts
+++ src/parsers/Attributes.ts
@@ -4,20 +4,55 @@
  * @return {array} // array of attributes from verbose
  **/
 
+const excludeTags = [
+  "class",
+  "style",
+  "data-widths",
+  "data-sizes",
+  "sizes",
+  "srcset",
+  "method",
+  "enctype",
+  "type",
+  "disabled",
+  "x",
+  "y",
+  "width",
+  "height",
+  "viewBox",
+  "fill",
+  "stroke-width",
+  "stroke-linecap",
+  "stroke-miterlimit",
+  "d",
+  "version",
+  "xmlns",
+  "xmlns:xlink",
+  "xml:space",
+  "transform",
+  "data-swaction",
+  "data-test",
+  "checked",
+  "data-image-widths",
+  "aria-hidden",
+]
 export declare type Attribute = {
-    name: string;
-    value: string;
+  name: string;
+  value: string;
 }
 export default (target: HTMLElement): { [key: string]: string; } => {
-    const response: { [key: string]: string; } = {};
-    for (const attr of target.attributes) {
+  const response: { [key: string]: string; } = {};
+  for (const attr of target.attributes) {
+    if (!excludeTags.includes(attr.name))
+      response[attr.name] = attr.value;
+  }
+  const childs = target.querySelectorAll('*');
+  for (const children of childs) {
+    for (const attr of children.attributes) {
+      if (!excludeTags.includes(attr.name))
         response[attr.name] = attr.value;
     }
-    const childs = target.querySelectorAll('*');
-    for (const children of childs) {
-        for (const attr of children.attributes) {
-            response[attr.name] = attr.value;
-        }
-    }
-    return response;
+  }
+  return response;
 }
+
