71df8ae5617dcd3d3924dd525c3803a2fcad15ac
simondelamarre <landscape.viewer@gmail.com>
2021-03-07 12:12:09 +0100

    try uncash cdn by renaming as beta version

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,7 @@ module.exports = env => {
       modules: ['node_modules', path.join(__dirname, 'src'), 'assets'],
     },
     output: {
-      filename: 'executePathSetup.min.js',
+      filename: 'executePathSetupBeta.min.js',
       path: path.resolve(__dirname, 'dist'),
       library: "pathScript",
       libraryTarget: "umd"
